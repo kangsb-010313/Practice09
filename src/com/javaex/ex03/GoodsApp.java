59e18a63ab477168d081fd9642d0ab0f4014a23f
kangsb-010313 <kangsb668@naver.com>
2025-05-14 23:14:47 +0900

    pt09 250514 23:15

--- src/com/javaex/ex03/GoodsApp.java
+++ src/com/javaex/ex03/GoodsApp.java
@@ -7,35 +7,6 @@ import java.util.Scanner;
 public class GoodsApp {
 
     public static void main(String[] args) {
-
-    	//스캐너
-    	
-    	//추상 클래스 List<Goods> gList = new ArrayList<Goods>();
-    	
-    	//sum = 0;
-    	
-    	//입력 (String, int, int) --> String으로 변경
-    	
-    	
-    	//상품을 입력해주세요(종료q)
-    	
-    	
-    	//for문
-    	//입력 시 "," 기준 배열 등록
-    	
-    	//사용자 입력
-    	
-    	//if (배열이 3개가 아닐 때)
-    	
-    	
-    	//else if(입력한 문자가 q일 때 종료)
-    	// 모든 상품의 갯수 => sum = sum + gList.get(i).getCount();
-    	//입력 완료 --> ======= --> showInfo --> sum
-    	//Goods
-    	//배열에 setName, setPrice, setCount 넣기
-    	
-    	//sc.close();
-    	
     	
     	Scanner sc = new Scanner(System.in);
     	
@@ -45,30 +16,41 @@ public class GoodsApp {
 		
     	System.out.println("상품을 입력해주세요(종료 q)");
     	
-    	
-    	for(int i=0; i<gList.size(); i++) {
-        	String info = sc.nextLine();
-        	String[] con = info.split(",");
-        	
-    		if(con.length != 3) {
-        		
-    		}else if(con.equals("q")){
-    			
-    			sum = sum + gList.get(i).getCount();
-    			
-    			System.out.println("입력완료");
-    			System.out.println("=================");
-    			gList.get(i).showInfo();
-    			System.out.println("모든 상품의 갯수는" + sum);
+
+    	while(true) { 
     		
+    		String info = sc.nextLine();
+    		
+    		String[] gArray = info.split(",");
+    		
+    		if(info.equals("q")) {
+    			break;
     		}
-    		
+    		/*
     		Goods g01 = new Goods();
-    		g01.setName(con[0]);
-    		g01.setPrice(Integer.parseInt(con[1]));
-    		g01.setCount(Integer.parseInt(con[2]));
+    		g01.setName(gArray[0]);
+    		g01.setPrice(Integer.parseInt(gArray[1]));
+    		g01.setCount(Integer.parseInt(gArray[2]));
+    		*/
+    		String name = gArray[0];
+    		int price = Integer.parseInt(gArray[1]);
+    		int count = Integer.parseInt(gArray[2]);
     		
-    	} 	
+    		Goods goods = new Goods(name, price, count);
+    		gList.add(goods);
+    		
+    		sum = sum + count; 
+    		
+    	}
+    	
+    	System.out.println("[입력완료]");
+    	System.out.println("=====================");
+    	
+    	for(Goods g : gList) {
+    		g.showInfo();
+    	}
+    	
+    	System.out.println("모든 상품의 갯수는 " + sum + " 개입니다.");
     	
     	sc.close();
     	
